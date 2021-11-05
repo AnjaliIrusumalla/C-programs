551d8fcf4385b760ce7fe42345e094a965a98335
Venkata Surya Kalanjali <82718813+AnjaliIrusumalla@users.noreply.github.com>
2021-11-04 23:49:48 -1200

    Add files via upload

---
+++ multiple variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a=123,b=30894;
+	float x=2.1346;
+	double dx=1.1412356;
+	char c='W';
+	printf("\na=%d",a);
+	printf("\nb=%d",b);
+	printf("\nx=%f",x);
+	printf("\ndx=%lf",dx);
+	printf("\nc=%d",c);
+}
---
+++ perimeter and area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int p,a;
+	
+	p=2*(7+5);
+	printf("\nPerimeter of the rectangle is:%dunits\n",p);
+	
+	a=7*5;
+	printf("\n Area of the rectangle is:%dunits\n",a);
+	
+	
+}
